c604bd4a14f47bb9e5ac27ca29468eaeb4e8748e
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-09-21 17:26:38 +0700

    Added UI

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -5,7 +5,9 @@ window.$ = window.jQuery = require("jquery");
 async function sendMethod(method, ...extraArgs) {
     return await ipcRenderer.invoke("mainWindow", method, extraArgs);
 }
- 
+
+var currentMenu = "";
+
 // function startMouseTracker() {
 //     requestAnimationFrame(startMouseTracker);
 
@@ -14,10 +16,44 @@ async function sendMethod(method, ...extraArgs) {
 //     $("#currentMouse").html(`MouseX: ${x} | MouseY: ${y}`);
 // }
 
+function switchMenu(menu) {
+    if (currentMenu !== "")
+        $(`.menu.${currentMenu}`).hide();
+
+    $(`.menu.${menu}`).show();
+
+    currentMenu = menu;
+}
+
 ipcRenderer.on("mainWindowMsg", (e, data) => {
     console.log(data);
 });
 
 window.onload = () => {
-    startMouseTracker();
+    switchMenu("main");
 };
+
+$("#btn_server").on("click", async () => {
+    await sendMethod("start server", 3011, "localhost", "testpass");
+    switchMenu("server");
+});
+
+$("#btn_client").on("click", () => {
+    switchMenu("client");
+});
+
+$("#btn_server_send_msg").on("click", async () => {
+    const msg = $("#input_server_send_msg").val();
+
+    await sendMethod("send message", msg);
+
+    $("#input_server_send_msg").val("");
+});
+
+$("#btn_client_send_msg").on("click", async () => {
+    const msg = $("#input_client_send_msg").val();
+
+    await sendMethod("send message", msg);
+
+    $("#input_client_send_msg").val("");
+});
